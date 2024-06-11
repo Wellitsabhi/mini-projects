f7befc7bbc0ac80ccb7cc4627ab4cf63591495e4
wellitsabhi <abhishekongit@gmail.com>
2024-06-11 16:43:13 +0530

    star-rating

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Accordion from './components/accordion'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,12 @@ function App() {
       {/* <Accordion/> */}
 
       {/* Random color componenet */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+
+      {/* Star Rating componenet */}
+      <StarRating noOfStars={7}/>
+
+      
     </div>
   )
 }
