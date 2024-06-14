bb20d88f0623a75e08192bee2468416f15f9fa10
wellitsabhi <abhishekongit@gmail.com>
2024-06-14 17:34:59 +0530

    image-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import React, {useState} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'/
 import './App.css'
-import Accordion from './components/accordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
-
+// import Accordion from './components/accordion'
+// import RandomColor from './components/random-color'
+// import StarRating from './components/star-rating'
+// import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-button'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
@@ -18,7 +19,13 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* Star Rating componenet */}
-      <StarRating noOfStars={7}/>
+      {/* <StarRating noOfStars={7}/> */}
+
+      {/* Image Slider Rating componenet */}
+      {/* <ImageSlider  url={'https://picsum.photos/v2/list'} limit={"10"} page={"1"} /> */}
+
+      {/* Load more data */}
+      < LoadMoreData />
 
       
     </div>
